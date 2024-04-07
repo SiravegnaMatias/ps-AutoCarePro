52d9c4ee31a4c7c6c3346064d27929c685f22908
MatiSrv <matisiravegna04@gmail.com>
2024-04-07 18:31:51 -0300

    Inicializacion de repositorio

---
+++ AutoCareProBE/src/main/java/utn/frc/ps/AutoCareProBE/AutoCareProBeApplication.java
@@ -0,0 +1,13 @@
+package utn.frc.ps.AutoCareProBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCareProBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCareProBeApplication.class, args);
+	}
+
+}
---
+++ AutoCareProBE/src/test/java/utn/frc/ps/AutoCareProBE/AutoCareProBeApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.frc.ps.AutoCareProBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCareProBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
